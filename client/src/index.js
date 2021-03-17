a2012f12172b2c0f7d41900e22272956e562f1b6
unknown <sudanbhusal20215@gmail.com>
2021-03-16 19:45:54 -0700

    Footer added

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const Footer = () => (
+  <footer className="footer">
+    <button onClick={()=> window.open("https://github.com/Sudan20215", "_blank")} colored>Sudan</button>
+    <button onClick={()=> window.open("https://github.com/lstvgore", "_blank")} colored>Lester</button>
+    <button onClick={()=> window.open("https://github.com/trukat", "_blank")} colored>Kathy</button>
+    <button onClick={()=> window.open("https://github.com/Frank-5850", "_blank")} colored>Franco</button>
+    <button onClick={()=> window.open("https://github.com/Nsilo", "_blank")} colored>Jvaka</button>
+  </footer>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App key="1" />, <Footer key="2" />
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
